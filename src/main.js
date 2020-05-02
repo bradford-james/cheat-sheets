72b622e888aee37171187f1d687994279717ba8e
bradford-james <bwettig@gmail.com>
2020-05-02 09:11:02 -0500

    Adjust production env var

--- src/main.js
+++ src/main.js
@@ -7,10 +7,9 @@ import routes from './routes';
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
-
 const router = new VueRouter({ routes, mode: 'history' });
 
 new Vue({
-  render: h => h(App),
-  router
+  render: (h) => h(App),
+  router,
 }).$mount('#app');
--- src/services/https-common.js
+++ src/services/https-common.js
@@ -1,7 +1,12 @@
 const axios = require('axios');
 
+const resourceURL =
+  process.env.NODE_ENV == 'production'
+    ? process.env.PRODUCTION_CORS_URL
+    : 'http://localhost:8080/api';
+
 module.exports = axios.create({
-  baseURL: process.env.PRODUCTION_CORS_URL || 'http://localhost:8080/api',
+  baseURL: resourceURL,
   // baseURL: 'https://cheat-sheets-api.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
