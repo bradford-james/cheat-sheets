6caa0e5eb6aab611c3584cb4239ac02016fc149e
bradford-james <bwettig@gmail.com>
2020-05-02 09:06:19 -0500

    Adjust production env var

--- src/services/https-common.js
+++ src/services/https-common.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 module.exports = axios.create({
-  // baseURL: process.env.PRODUCTION_CORS_URL || 'http://localhost:8080/api',
-  baseURL: 'https://cheat-sheets-api.herokuapp.com/',
+  baseURL: process.env.PRODUCTION_CORS_URL || 'http://localhost:8080/api',
+  // baseURL: 'https://cheat-sheets-api.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
