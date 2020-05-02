2f3d8c305e9bb5a79b0f9a8d09f0dd699e675dd4
bradford-james <bwettig@gmail.com>
2020-05-02 09:19:09 -0500

    Added production env var

--- src/services/https-common.js
+++ src/services/https-common.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const resourceURL =
   process.env.NODE_ENV == 'production'
-    ? process.env.PRODUCTION_CORS_URL
+    ? process.env.VUE_APP_PRODUCTION_CORS_URL
     : 'http://localhost:8080/api';
 
 module.exports = axios.create({
